06c487d3ef08535fa8801e4031f85aa273e2e25c
FlorentVASSEURBERLIOUX <vasseurberlioux.florent@gmail.com>
2025-01-31 21:10:29 +0100

    Premier test de la librairie

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chess-AI-Learning
+
+
+*Commande d'execution du code :*
+java -jar target/chess-ai-learning-1.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.move.Move;
-import javax.swing.SwingUtilities;
+//import javax.swing.SwingUtilities; // plus tard
 
 public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println(board.toString());
 
+        Move move1 = new Move("e2e4", board.getSideToMove());
+        board.doMove(move1);
+
+        System.out.println(board.toString());
+
+        Move move2 = new Move("c7c6", board.getSideToMove());
+        board.doMove(move2);
+
+        System.out.println(board.toString());
+
+        System.out.println(board.getFen());
+    }
 }
