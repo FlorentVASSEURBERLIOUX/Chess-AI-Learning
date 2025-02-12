d75163a15899177c05f10168818ef357bc790c60
FlorentVASSEURBERLIOUX <vasseurberlioux.florent@gmail.com>
2025-02-12 18:29:25 +0100

    Finalisation de la methode de sauvegarde des parties

--- src/main/java/Partie.java
+++ src/main/java/Partie.java
@@ -1,3 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 import com.github.bhlangonijr.chesslib.Board;
 import com.github.bhlangonijr.chesslib.move.Move;
 import com.github.bhlangonijr.chesslib.move.MoveList;
@@ -7,7 +14,7 @@ public class Partie {
     private JoueurIA joueurNoir;
     private JoueurIA joueurBlanc;
     private Generation generation;
-    private MoveList historique;
+    public MoveList historique;
 
     public Partie(JoueurIA joueurNoir, JoueurIA joueurBlanc, Generation generation) {
         this.joueurNoir = joueurNoir;
@@ -22,46 +29,54 @@ public class Partie {
         historique.add(coup);
     }
 
-public String getPGNPartie() {
-    String PGN = "[Event \"Live Chess\"]\n" +
-                 "[Site \"https://github.com/FlorentVASSEURBERLIOUX\"]\n" +
-                 "[Date \"0001.01.01\"]\n" +
-                 "[Round \"?\"]\n" +
-                 "[White \"Gen" + generation.getNGeneration() + " N°" + getClassementJoueur(joueurBlanc) + "\"]\n" +
-                 "[Black \"Gen" + generation.getNGeneration() + " N°" + getClassementJoueur(joueurNoir) + "\"]\n" +
-                 "[Result \"1/2-1/2\"]\n" +
-                 "[TimeControl \"600\"]\n" +
-                 "[WhiteElo \"" + joueurBlanc.getRecompense() + " pts\"]\n" +
-                 "[BlackElo \"" + joueurNoir.getRecompense() + " pts\"]\n" +
-                 "[Termination \"Suspense\"]\n" +
-                 "[ECO \"B10\"]\n" +
-                 "[EndTime \"00:00:00 PST\"]\n" +
-                 "[Link \"https://github.com/FlorentVASSEURBERLIOUX\"]\n\n";
-    
-    int coupNum = 1;
-    for (int i = 0; i < historique.size(); i++) {
-        if (i % 2 == 0) {
-            PGN += coupNum + ". ";
-            coupNum++;
+    public String getPGNPartie() {
+        String historiqueSAN = historique.toSan();
+
+        String PGN = "[Event \"Live Chess\"]\n" +
+                "[Site \"https://github.com/FlorentVASSEURBERLIOUX\"]\n" +
+                "[Date \"0001.01.01\"]\n" +
+                "[Round \"?\"]\n" +
+                "[White \"Gen" + generation.getNGeneration() + " N°" + getClassementJoueur(joueurBlanc) + "\"]\n" +
+                "[Black \"Gen" + generation.getNGeneration() + " N°" + getClassementJoueur(joueurNoir) + "\"]\n" +
+                "[Result \"1/2-1/2\"]\n" +
+                "[TimeControl \"600\"]\n" +
+                "[WhiteElo \"" + joueurBlanc.getRecompense() + " pts\"]\n" +
+                "[BlackElo \"" + joueurNoir.getRecompense() + " pts\"]\n" +
+                "[Termination \"Suspense\"]\n" +
+                "[ECO \"B10\"]\n" +
+                "[EndTime \"00:00:00 PST\"]\n" +
+                "[Link \"https://github.com/FlorentVASSEURBERLIOUX\"]\n\n";
+
+        if (echequier.isDraw()) {
+            PGN += historiqueSAN + " 1/2-1/2\n";
+
+        } else if (echequier.isMated()) {
+            PGN += historiqueSAN + " 1-0\n";
+
+        } else {
+            PGN += historiqueSAN + " 0-1\n";
+
         }
-        PGN += historique.get(i).toString() + " ";
+        return PGN;
     }
-    PGN += "1/2-1/2\n";  
-    return PGN;
-}
 
     public int getClassementJoueur(JoueurIA joueur) {
         return generation.getClassementJoueurs().indexOf(joueur) + 1;
     }
 
+    public Board getEchequier() {
+        return echequier;
+    }
+
     public void sauvegarderPGN() {
+        File saveDir = new File("../save");
+        saveDir.mkdirs();
+
         File fichier = new File("../save/Gen" + generation.getNGeneration() + ".pgn");
-        
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fichier));
             writer.write(getPGNPartie());
             writer.close();
-            
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
